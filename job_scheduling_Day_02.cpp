f0d17f8204be5a4ce2cfa5acca5db36c212ed9cb
milangeorge98 <milangeorge98@gmail.com>
2020-10-01 12:19:21 +0530

    adding greedy

---
+++ job_scheduling_Day_02.cpp
@@ -0,0 +1,53 @@
+//JOB SCHEDULING ALGORITHM
+
+#include <iostream>
+
+using namespace std;
+
+struct Job{
+	char id;
+	int deadline;
+	int profit;
+};
+
+bool compare(Job a, Job b){
+	return (a.profit > b.profit);
+}
+
+void printJobsAccProfit(Job arr[], int n)
+{
+	int result[n];
+	bool slot[n];
+	
+	for(int i=0; i <n; i++){
+		slot[i]=false;
+	}
+	
+	for(int i=0; i <n; i++)
+	{
+		for(int j=min(n, arr[i].deadline)-1; j>=0; j--)
+		{
+			if(slot[j]==false)
+			{
+				result[j]=i;
+				slot[j]=true;
+				break;
+			}
+		}
+	}
+	
+	for (int i=0; i<n; i++) 
+       if (slot[i]) 
+         cout << arr[result[i]].id << " "; 
+		
+}
+
+int main() 
+{ 
+    Job arr[] = { {'a', 2, 100}, {'b', 1, 19}, {'c', 2, 27}, 
+                   {'d', 1, 25}, {'e', 3, 15}}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    cout << "Following is maximum profit sequence of jobsn"; 
+    printJobsAccProfit(arr, n); 
+    return 0; 
+} 
