074da2ff52b453763d664768cd33460868b6b8a6
milangeorge98 <milangeorge98@gmail.com>
2020-12-19 18:49:02 +0530

    Spiral Traversal of a 2D array

---
+++ Day_21_Spiral_Traversal.cpp
@@ -0,0 +1,44 @@
+using namespace std;
+
+vector<int> spiralTraverse(vector<vector<int>> array) {
+  // Write your code here.
+	
+	vector<int> result={};
+	int sR =0;
+	int eR = array.size()-1;
+	int sC =0;
+	int eC = array[0].size()-1;
+	
+	while(sR <=eR && sC <=eC)
+	{
+		for(int col = sC; col <=eC; col++)
+		{
+			result.push_back(array[sR][col]);
+		}
+		
+		for(int row =sR+1; row <=eR; row++)
+		{
+			result.push_back(array[row][eC]);
+		}
+		for(int col = eC-1; col >= sC; col--)
+		{
+			if(sR==eR)
+				break;
+			result.push_back(array[eR][col]);
+		}
+		
+		for(int row=eR-1; row > sR; row--)
+		{
+			if(sC==eC)
+				break;
+		 result.push_back(array[row][sC]);
+		}
+		
+		sR++;
+		eR--;
+		sC++;
+		eC--;
+		
+	}
+  return result;
+}
