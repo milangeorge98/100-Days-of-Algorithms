792dfe5bc140841e1e5ccc00f9adaf536654d81d
Milan George <milangeorge98@gmail.com>
2021-03-29 18:34:55 +0530

    Generates all subsets from the given array

---
+++ Day_28_Powerset.py
@@ -0,0 +1,8 @@
+def powerSet(array):
+	subsets =[[]]
+	for element in array:
+		for i in range(len(subsets)):
+			currentSubset = subsets[i]
+			subsets.append(currentSubset+[element])
+    
+	return subsets
